9f485c5c54f823ae006a105334b34f077aa9c618
Sam Harwell <sam@tunnelvisionlabs.com>
2014-03-18 13:11:20 -0500

    Updated build for release 1.0.0-alpha001

--- Dev10/push.ps1
+++ Dev10/push.ps1
@@ -1,8 +1,10 @@
-$Version = "1.0.0-alpha001"
+. .\version.ps1
 
-.\nuget\NuGet.exe push .\nuget\release\Microsoft.VisualStudio.Project.10.0.nupkg
+If ($Version.EndsWith('-dev')) {
+	Write-Host "Cannot push development version '$Version' to NuGet."
+	Exit 1
+}
 
-## For now, the Visual Studio 2012 project doesn't include any additional features,
-## so there's no reason for users to choose it. Uploading it at this point would
-## simply be misleading.
-# .\nuget\NuGet.exe push .\nuget\release\Microsoft.VisualStudio.Project.11.0.nupkg
+.\nuget\NuGet.exe 'push' ".\nuget\release\Microsoft.VisualStudio.Project.10.0.$Version.nupkg"
+.\nuget\NuGet.exe 'push' ".\nuget\release\Microsoft.VisualStudio.Project.11.0.$Version.nupkg"
+.\nuget\NuGet.exe 'push' ".\nuget\release\Microsoft.VisualStudio.Project.12.0.$Version.nupkg"
