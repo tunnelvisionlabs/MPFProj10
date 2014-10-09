d14436b621118cfba1f44baeab0c4afb28698d88
Sam Harwell <sam.harwell@rackspace.com>
2014-10-09 15:05:58 -0500

    Add ProvideFilterAttribute registration attribute (fixes #33)

---
+++ Dev10/Src/CSharp/FilterBrowsableState.cs
@@ -0,0 +1,26 @@
+﻿namespace Microsoft.VisualStudio.Project
+{
+    using System;
+
+    /// <summary>
+    /// Specifies the priority for a file filter within a particular context.
+    /// </summary>
+    /// <seealso cref="ProvideFilterAttribute"/>
+    public enum FilterBrowsableState
+    {
+        /// <summary>
+        /// The file filter is not visible.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The file filter is visible, but after any <see cref="Common"/> items.
+        /// </summary>
+        Standard,
+
+        /// <summary>
+        /// The file filter is visible, and placed before any <see cref="Standard"/> filters.
+        /// </summary>
+        Common,
+    }
+}
